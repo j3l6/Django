042f017f72694371c89108d941f725ef83be5e0a
jelg <perpetualmoti0n@outlook.com>
2025-06-10 13:54:10 -0600

    Views modified, 2 views added

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import Http404
 
+def post_list(request):
+    posts = Post.published.all()
+    return render(
+        request,
+        'blog/post/list.html',
+        {'posts':posts}
+    )
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExiste:
+        raise Http404("No Post found :(.")
+    return render(
+        request,
+        'blog/post/list.html',
+        {'posts':posts}
+    )
 # Create your views here.
+
+
